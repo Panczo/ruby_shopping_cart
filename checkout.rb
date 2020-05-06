d4c2e491e3a8ad6904016c35833f96f53684d383
pnc <joachimsantana@gmail.com>
2020-05-06 22:14:44 +0200

    Refactor checkout class and remove promotional rules module

--- checkout.rb
+++ checkout.rb
@@ -1,14 +1,53 @@
-promotional_rules = {
-  individual: [
-    { product_code: '001', rule: '> 1', new_price: '8.5' }
-  ],
-  total: [
-    { rule: '> 60', new_price: '10%' }
-  ]
-}
-
 class Checkout
-  def initialize(promotional_rules)
-    @promotional_rules = promotional_rules
+  def initialize(checkout_rules)
+    @checkout_rules = checkout_rules
+    @items = []
+  end
+
+  def scan(item)
+    @items << item
+  end
+
+  def total
+      appropriate_rules = @checkout_rules.select {|rule| rule[:predicate].call(@items) }
+      appropriate_rules.each {|rule| rule[:apply].call(@items) }
+      puts @items.inspect
+      @items.sum {|i| i[:price] }
   end
 end
+
+rules = [
+  {
+    predicate: -> (items) { items.select {|i| i[:name] == 'Red Scarf' }.size >= 2 },
+    apply: -> (items)     { items.select {|i| i[:name] == 'Red Scarf' }.each { |i| i[:price] = 8.5 } }
+  },
+  {
+    predicate: -> (items) { items.map { |i| i[:price] }.sum > 60 },
+    apply: -> (items)     { items.each { |i| (i[:price] *= 0.9).round(2) } }
+  }
+]
+
+items = [
+  {
+    id: '001',
+    name: 'Red Scarf',
+    price: 9.25
+  },
+  {
+    id: '002',
+    name: 'Silver cufflinks',
+    price: 45.00
+  },
+  {
+    id: '003',
+    name: 'Silk Dress',
+    price: 19.95
+  }
+]
+
+co = Checkout.new(rules)
+co.scan(items[0])
+co.scan(items[1])
+co.scan(items[0])
+co.scan(items[2])
+puts co.total
